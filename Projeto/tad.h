f16d6b32417e74f958d65040c91f7e63ad4fa716
matheusmda <80073708+matheusmda@users.noreply.github.com>
2021-05-26 12:52:13 -0300

    Alguns Refinamentos e Correção de Bug Encontrado
    Próximos passos:
    
    1 - Melhorar interface gráfica!!
    
    2 - Ler todo o código:
    	Checar todos os comentários
    	Checar todos os nomes de variáveis
    	Checar gramática
    	Checar todos os nomes de funções
    
    ====================================================================================
    
    Mudanças:
    
    1 - Tirei os contadores numElem de todas as listas, consequentemente, de todo o código, já que era desnecessário!! -> Estou enxugando e refinando o código!!
    
    2 - Arrumei um erro que encontrei na hora de alterar o medicamento, especificamante quando o usuário tenta alterar o tipo do medicamento, isto é, trocar o medicamento de lista (trocar o tipo, no código, é trocar de lista). Eu estava deletando o nó ao invés de só cortar o ligamento na lista a qual ele não pertencia mais. E aí, na hora de mostrar esse nó na execução do programa, ficava sem as informações dele, não só isso, estava dando double free, pois na hora de percorrer, tinha 2 ponteiros apontando pra ele... Então mudei minha função retirarDuplicata!!
    
    ------------------------------------------------------------------------------------

--- Projeto/tad.h
+++ Projeto/tad.h
@@ -3,12 +3,17 @@
 #include <string.h>
 #define max 50
 
+// TAD Medicação
 typedef struct remedio med;
 
+// TAD Tipo Medicação
 typedef struct tipo_remedio tipo;
 
+// TAD Lista - Estrutura de Dados Lista
 typedef struct lista lista;
 
+// Função de inicialização da estrutura de dados lista
 lista* criaLista();
 
-void menuPrincipal(int contMenuPrincipal, lista* l); // menuzão que eu vou sempre voltar pra ele
+// Menu com as principais funcionalidades do programa
+void menuPrincipal(int contMenuPrincipal, lista* l);
